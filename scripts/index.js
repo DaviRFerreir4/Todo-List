f50a781ee52d88bda09ddbe64c7875c4d5e03fc8
Davi Ferreira <46787385+DaviRFerreir4@users.noreply.github.com>
2025-06-23 13:44:06 -0300

    Funcionalidade de criação de todos implementada

--- scripts/index.js
+++ scripts/index.js
@@ -13,21 +13,10 @@ document.querySelector("#changeMode").addEventListener("click", () => {
 
 let editTodo = document.querySelectorAll("svg.edit");
 editTodo.forEach((element, index) => {
-  let inputText = document.querySelectorAll("input[type=text]")[index + 1];
   let inputCheckbox = document.querySelectorAll("input[type=checkbox]")[index + 1];
+  let inputText = document.querySelectorAll("input[type=text]")[index + 1];
   element.addEventListener("click" , () => {
-    if (inputText.disabled) {
-      inputText.disabled = false;
-      inputText.focus();
-      inputText.style.borderColor = "var(--todo-font-color)";
-      inputCheckbox.disabled = true;
-      inputCheckbox.style.backgroundColor = "var(--todo-border-color)";
-    } else {
-      inputText.disabled = true;
-      inputText.style.borderColor = "transparent";
-      inputCheckbox.disabled = false;
-      inputCheckbox.style.backgroundColor = "transparent";
-    }
+    editElement(inputCheckbox, inputText);
   });
 });
 
@@ -35,6 +24,80 @@ let deleteTodo = document.querySelectorAll("svg.delete");
 deleteTodo.forEach((element, index) => {
   let todoWrapper = document.querySelectorAll("div.todo-wrapper")[index + 1];
   element.addEventListener("click", () => {
-    todoWrapper.remove();
+    deleteElement(todoWrapper);
   });
 });
+
+let addTodo = document.querySelector("svg.add");
+addTodo.addEventListener("click", () => {
+  let newTodo = document.createElement("div");
+  newTodo.classList.add("todo-wrapper");
+  let inputCheckbox = document.createElement("input");
+  inputCheckbox.type = "checkbox";
+  let inputText = document.createElement("input");
+  inputText.type = "text";
+  inputText.value = document.querySelector("input[type=text]").value;
+  inputText.disabled = true;
+  svgEdit = createSvgEdit();
+  svgEdit.addEventListener("click", () => {
+    editElement(inputCheckbox, inputText);
+  });
+  newTodo.append(inputCheckbox);
+  newTodo.append(inputText);
+  let div = document.createElement("div");
+  svgDelete = createSvgDelete();
+  svgDelete.addEventListener("click", () => {
+    deleteElement(newTodo);
+  });
+  div.append(svgEdit);
+  div.append(svgDelete);
+  newTodo.append(div);
+  document.querySelector("div.todo-list").prepend(newTodo);
+  document.querySelector("input[type=text]").value = "";
+});
+
+function editElement(inputCheckbox, inputText) {
+  if (inputText.disabled) {
+    inputText.disabled = false;
+    inputText.focus();
+    inputText.style.borderColor = "var(--todo-font-color)";
+    inputCheckbox.disabled = true;
+    inputCheckbox.style.backgroundColor = "var(--todo-border-color)";
+  } else {
+    inputText.disabled = true;
+    inputText.style.borderColor = "transparent";
+    inputCheckbox.disabled = false;
+    inputCheckbox.style.backgroundColor = "transparent";
+  }
+}
+
+function deleteElement(element) {
+  element.remove();
+}  
+
+function createSvgEdit() {
+  let svgEdit = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  svgEdit.setAttribute("width", "24");
+  svgEdit.setAttribute("height", "24");
+  svgEdit.setAttribute("fill", "var(--todo-font-color)");
+  svgEdit.setAttribute("viewBox", "0 0 256 256");
+  svgEdit.classList.add("add");
+  let path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+  path.setAttribute("d", "M232.49,55.51l-32-32a12,12,0,0,0-17,0l-96,96A12,12,0,0,0,84,128v32a12,12,0,0,0,12,12h32a12,12,0,0,0,8.49-3.51l96-96A12,12,0,0,0,232.49,55.51ZM192,49l15,15L196,75,181,60Zm-69,99H108V133l56-56,15,15Zm105-7.43V208a20,20,0,0,1-20,20H48a20,20,0,0,1-20-20V48A20,20,0,0,1,48,28h67.43a12,12,0,0,1,0,24H52V204H204V140.57a12,12,0,0,1,24,0Z");
+  svgEdit.append(path);
+  return svgEdit;
+}
+
+function createSvgDelete() {
+  let svgDelete = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  svgDelete.setAttribute("xmlns", "http://www.w3.org/2000/svg")
+  svgDelete.setAttribute("width", "24")
+  svgDelete.setAttribute("height", "24")
+  svgDelete.setAttribute("fill", "var(--todo-font-color)")
+  svgDelete.setAttribute("viewBox", "0 0 256 256")
+  svgDelete.setAttribute("class", "delete")
+  let path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+  path.setAttribute("d", "M228,128a12,12,0,0,1-12,12H40a12,12,0,0,1,0-24H216A12,12,0,0,1,228,128Z");
+  svgDelete.append(path);
+  return svgDelete;
+}
